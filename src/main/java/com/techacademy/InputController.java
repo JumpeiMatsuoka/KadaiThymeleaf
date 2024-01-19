0eb346a3f3e70e6b2d402267c02353d684e69eb1
JumpeiMatsuoka <totoro84@hotmail.co.jp>
2024-01-19 11:45:57 +0900

    課題修正

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -2,16 +2,23 @@ package com.techacademy;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class InputController {
 
-    @GetMapping("/input")
-    public String getInput(@RequestParam(name = "val", required = false) String val, Model model) {
+ // 入力画面の初期表示をする
+ @GetMapping("/input")
+ public String getInput(@RequestParam(required = false) String previous, Model model) {
+     // 'previous'パラメータがあればモデルに追加
+     model.addAttribute("previous", previous);
+     return "input";
+     }
 
-    return "input";
-    }
+ // 入力画面からのデータを受け取り、出力画面を表示する
+ @PostMapping("/output")
+ public String postOutput(@RequestParam("inputValue") String inputValue, Model model) {
+     model.addAttribute("inputValue", inputValue);
+     return "output";
+     }
 }
-
